71b7a913fad3e52464a8a9a4ae88165e85a45663
edoardolai <edoardolai17@gmail.com>
2024-10-24 14:11:57 +0200

    Creation of Table class, __str__ added to Seat

--- utils/table.py
+++ utils/table.py
@@ -1,9 +1,13 @@
+from typing import List
 class Seat:
     def __init__(self, free:bool, occupant:str):
         self.free=free
         self.occupant=occupant
         if self.occupant:
             self.free=False
+    
+    def __str__(self):
+       return f"A seat object with free state: {self.free} and occupied by {self.occupant}"
 
     def set_occupant(self,name:str):
         if self.free:
@@ -20,4 +24,29 @@ class Seat:
 
         else:
             print('The seat is already empty')
+
+
+class Table:
+    def __init__(self,capacity: int, seats: List[Seat]): #Todo: create fall-back if len(seats) > capacity
+        self.capacity = capacity
+        self.seats = seats
+        
+    def __str__(self):
+        return f"A Table object with a capacity of {self.capacity}, and with the following members: {', '.join([seat.occupant for seat in self.seats])}"
+
+    def has_free_spot(self) -> bool:
+        return self.letf_capacity() > 0
+
+    def assign_seat(self, name: str):
+        if self.has_free_spot():
+            seat = Seat(True, name)
+            self.seats.append(seat)
+        else:
+            print('Sorry, this table is currently full!')
+    
+    def letf_capacity(self) -> int:
+        return self.capacity - len(self.seats)
+
+
+
 
