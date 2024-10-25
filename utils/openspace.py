c10ab3a7cce3587a17329abedf3ab70906e1c641
edoardolai <edoardolai17@gmail.com>
2024-10-25 11:47:48 +0200

    Create new attribute for Openspace table_disposition for openspace class

--- utils/openspace.py
+++ utils/openspace.py
@@ -8,7 +8,6 @@ class Openspace:
     def __init__(self, tables: List[Table], number_of_tables: int):
         self.tables = tables
         self.number_of_tables = number_of_tables
-    
     def __str__(self):
         return f"A open space object with the following tables disposition:\n{' '.join([table.__str__() for table in self.tables])}"  #todo complete description
     
@@ -19,12 +18,13 @@ class Openspace:
                 if table.has_free_spot():
                     assigned_student = names[table_nb + seat_nb]
                     table.assign_seat(assigned_student)
-    
-    def store(self, file_name: str):
         tables_disposition = [[student.occupant for student in table.seats] for table in self.tables ]
-        table_names = [f"Table {i + 1}" for i,table in enumerate(self.tables)]
+        table_names = [f"Table {i + 1}" for i in range(len(self.tables))]
         tables_df = pd.DataFrame(tables_disposition).T
         tables_df.columns = table_names
-        tables_df.to_csv(file_name)
+        self.room_disposition = tables_df
+    
+    def store(self, file_name: str):
+        self.room_disposition.to_csv(file_name)
 
 
